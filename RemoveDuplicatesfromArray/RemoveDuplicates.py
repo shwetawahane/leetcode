76d197cd022524a984e6b9081eb013cb23938d90
shwetawahane <48511840+shwetawahane@users.noreply.github.com>
2021-06-04 08:34:00 -0400

    Update RemoveDuplicates.py

--- RemoveDuplicatesfromArray/RemoveDuplicates.py
+++ RemoveDuplicatesfromArray/RemoveDuplicates.py
@@ -9,3 +9,17 @@ def removeDuplicates(self, nums: List[int]) -> int:
                 i+=1
         return len(nums)
       
+#Alternate Java implementation
+"""
+public int removeDuplicates(int[] nums) {
+    if (nums.length == 0) return 0;
+    int i = 0;
+    for (int j = 1; j < nums.length; j++) {
+        if (nums[j] != nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+}
+"""
